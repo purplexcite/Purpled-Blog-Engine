8f44fc4fe22176a35bef72af9a002f20b0f4ede9
purple <master@localhost.(none)>
2010-08-29 02:13:49 +0300

    added function close_tags

--- application/views/index.php
+++ application/views/index.php
@@ -28,10 +28,48 @@
             {
                 $model = new Model_Comment;
 
+                // Function from http://wap.blog.infan.ru/alex/1/16/?B=493
+                function close_tags(&$s, $tags)
+                {
+                    $p = 0;
+                    $open = array();
+
+                    while (($p = strpos($s, '<', $p)) !== false) {
+                        if (preg_match("~^<(/?)([a-z\d]+)\b[^>]*>~i", substr($s, $p), $m)) {
+                        $tag = strtolower($m[2]);
+
+                        if (preg_match("/^($tags)$/", $tag)) {
+                            if ($m[1]) { // </tag>
+                            if ($open[$tag] > 0)
+                                $open[$tag]--;
+                            else { // удалить лишний закрывающий тег
+                                $s = substr_replace($s, '', $p, strlen($m[0]));
+                                continue;
+                            }
+                            }
+                            else
+                            $open[$tag]++;
+                        }
+
+                        $p += strlen($m[0]);
+                        }
+                        else
+                        $p++;
+                    }
+
+                    foreach ($open as $tag => $p)
+                        if ($p > 0)
+                        $s .= "</".$tag.">";
+                    }
+
                 foreach($posts as $post)
                 {
+                    $message = nl2br(mb_substr($post->content, 0, 25));
+
+                    close_tags($message, '(b|u|i)');
+
                     echo '<li><h3>&laquo;'.$post->title.'&raquo; &mdash; '.$post->posted.', '.$post->username.'</h3></li>';
-                    echo '<li>'.nl2br(strip_tags(mb_substr($post->content, 0, 25))).'...</li>';
+                    echo '<li>'.$message.'...</li>';
                     echo '<li>&nbsp;</li>';
                     echo '<li>'.($post->allowcomment == 1 ? '<i>Комментариев: '.$model->get_count($post->id).'</i>' : '<i>Комментирование запрещено</i>').'</li>';
                     echo '<li>'.html::anchor('post/'.$post->url.'-'.$post->id, 'Читать далее').'</li>';
